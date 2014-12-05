670036f61eda01e5cd2d5a8dd3b2d0d495175782
arlinajsk <karen.b.lin@gmail.com>
2014-12-05 20:48:25 +0000

    adding data model

---
+++ tianren/datamodel/enums.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+zero        = '0'
+one         = '1'
+two         = '2'
+three       = '3'
+four        = '4'
+five        = '5'
+six         = '6'
+seven       = '7'
+eight       = '8'
+nine        = '9'
+ten         = '10'
+eleven      = '11'
+twelve      = '12'
+thirteen    = '13'
+fourteen    = '14'
+fifteen     = '15'
+sixteen     = '16'
+seventeen   = '17'
+eighteen    = '18'
+ninteen     = '19'
+twenty      = '20'
+twentyone   = '21'
+twentytwo   = '22'
+twentythree = '23'
+twentyfour  = '24'
+twentyfive  = '25'
+twentysix   = '26'
+twentyseven = '27'
+twentyeight = '28'
+twentynine  = '29'
+thirty      = '30'
+thirtyone   = '31'
+
+male        = 'M'
+female      = 'F'
+
+
+MONTHS = (
+    ( one,      u'一' ),    ( two,    u'二'),  ( three,    u'三'),      ( four,     u'四'),
+    ( five,     u'五' ),    ( six,    u'六'),  ( seven,    u'七'),      ( eight,     u'八'),
+    ( nine,     u'九' ),    ( ten,      u'十'),  ( eleven,    u'十一'),   (twelve,     u'十二'),
+)
+
+DATES = (
+    ( one,      u'一' ),     ( two,      u'二'),      ( three,    u'三'),      ( four,     u'四'),
+    ( five,      u'五' ),    ( six,      u'六'),      ( seven,    u'七'),      ( eight,     u'八'),
+    ( nine,      u'九' ),    ( ten,      u'十'),      ( eleven,    u'十一'),   (twelve,     u'十二'),
+    ( thirteen,  u'十三'),   ( fourteen,  u'十四'),   ( fifteen,  u'十五'),    ( sixteen,  u'十六'), 
+    ( seventeen, u'十七'),   ( eighteen,  u'十八'),   ( ninteen,  u'十九'),    ( twenty,  u'二十'), 
+    ( twentyone, u'廿一'),   ( twentytwo, u'廿二'),   ( twentythree,  u'廿三'),    ( twentyfour,  u'廿四'), 
+    ( twentyfive, u'廿五'),   ( twentysix, u'廿六'),  ( twentyseven,  u'廿七'),    ( twentyeight,  u'廿八'), 
+    ( twentynine, u'廿九'),   ( thirty, u'三十'),     ( thirtyone,  u'三十一'), 
+
+)
+
+
+
+
+HOURS = (
+    ( zero, u'子'  ),
+    ( one,  u'丑' ),  ( two,  u'寅' ), ( three,    u'卯'), ( four,     u'辰'),
+    ( five, u'巳' ),  ( six,  u'午'),  ( seven,    u'未'), ( eight,    u'申'),
+    ( nine, u'酉' ),  ( ten,  u'戌'),  ( eleven,   u'亥'), 
+)
+
+GENDER = (
+
+    ( male, u'乾' ), (female, u'坤'),    
+)
+
+MONTHLY     = 'MY'
+QUARTERLY   = 'QY'
+DAILY       = 'DY'
+ANNUALY     = 'AY'
+
+ROUTINE_FREQUENCY = (
+    ( MONTHLY,      'Monthly' ),
+    ( QUARTERLY,    'Quarterly'),
+    ( ANNUALY,      'Annualy'),
+    ( DAILY,        'Daily'),
+)
---
+++ tianren/datamodel/models.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+from tianren.datamodel.enums import MONTHS, DATES, HOURS, GENDER, ROUTINE_FREQUENCY
+
+class Temple( models.Model ):
+    templeId    = models.AutoField(primary_key=True)
+    templeName  = models.CharField(max_length=128, verbose_name=u'佛堂')
+    address1    = models.CharField(max_length=128)
+    address2    = models.CharField(max_length=128, blank=True, null=True)
+    city        = models.CharField(max_length=64)
+    state       = models.CharField(max_length=2)
+    zipCode     = models.IntegerField()
+    
+    def __unicode__( self ):
+        return self.templeName
+    
+class People( models.Model ):
+    personId    = models.AutoField(primary_key=True)
+    lastName    = models.CharField(max_length=64)
+    firstName   = models.CharField(max_length=64)
+    chineseName = models.CharField(max_length=64, blank=True, verbose_name=u'中文名')
+    gender      = models.CharField(max_length=1, choices=GENDER, default='M', verbose_name=u'乾/坤')
+    address1    = models.CharField(max_length=128)
+    address2    = models.CharField(max_length=128, blank=True, null=True)
+    city        = models.CharField(max_length=64)
+    state       = models.CharField(max_length=2)
+    zipCode     = models.CharField(max_length=10)
+    homePhone   = models.CharField(max_length=10, blank=True, null=True)
+    cellPhone   = models.CharField(max_length=10, blank=True, null=True)
+    email       = models.EmailField(blank=True)
+    language    = models.CharField(max_length=32, blank=True, null=True )
+    transport   = models.CharField(max_length=16, blank=True, null=True )
+    notes       = models.TextField(blank=True)
+
+    def __unicode__( self ):
+        return u'{0},{1} ( {2} )'.format( self.lastName, self.firstName, self.chineseName )
+
+class Initiation( models.Model ):
+    personId                = models.ForeignKey( People, primary_key=True )
+    initiationDate          = models.DateField(verbose_name='西元')
+    initiationChineseYear   = models.CharField(max_length=8, verbose_name='歲次')
+    initiationChineseMonth  = models.CharField(max_length=2, choices=MONTHS, verbose_name='月')
+    initiationChineseDay    = models.CharField(max_length=2, choices=DATES, verbose_name='日')
+    initiationChineseHour   = models.CharField(max_length=2, choices=HOURS, verbose_name='時辰')
+    templeName              = models.ForeignKey( Temple )
+    dianChuanShr            = models.CharField(max_length=8, verbose_name='點傳師')
+    introductor             = models.ForeignKey( People, related_name='initiation_introductor', verbose_name='引師' )
+    guarantor               = models.ForeignKey( People, related_name='initiation_guarantor', verbose_name='保師')
+    donationAmount          = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='功德費')
+    initiationNotes         = models.TextField(blank=True, null=True)
+    
+    def __unicode__( self ):
+        return u'{0}, {1}, {2}, {3}, {4}'.format( self.personId, self.initiationDate, self.introductor, self.guarantor, self.dianChuanShr  ) 
+    
+class Donation( models.Model ):
+    donationId          = models.AutoField(primary_key=True)
+    personId            = models.ForeignKey( People )
+    donationDate        = models.DateField()
+    donationAmount      = models.DecimalField(decimal_places=2, max_digits=10, verbose_name='功德費')
+    routineDonation     = models.NullBooleanField(default='No')
+    routineFrequency    = models.CharField( max_length=2, choices=ROUTINE_FREQUENCY, blank=True, null=True)
+    donationNotes       = models.TextField(blank=True, null=True)
+    
+    def __unicode__( self ):
+        return u'{0}, {1}, {2}, {3}'.format( self.donationId, self.personId, self.donationDate, self.donationAmount )
+        
+        
